5a2d68bc86073139c0166db4837d755d33f423c5
Artem <dt2csgo@mail.ru>
2024-10-09 02:00:21 +0400

    feat: update model for event and add new category in api info

--- models/eventModel.js
+++ models/eventModel.js
@@ -5,7 +5,15 @@ const { DataTypes } = require('sequelize')
 
 // TODO:Написать модель для мероприятий
 const EventModel = db.define('event', {
-
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true, allowNull: false},
+    name: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false},
+    city: {type: DataTypes.STRING, allowNull: false},
+    district: {type: DataTypes.STRING, allowNull: false},
+    street: {type: DataTypes.STRING, allowNull: false},
+    dueTime: {type: DataTypes.DATE, allowNull: false},
+    fundraising: {type: DataTypes.BOOLEAN, allowNull: false},
+    totalAmount: {type: DataTypes.INTEGER, allowNull: false},
 })
 
 module.exports = EventModel;
--- models/participantsModel.js
+++ models/participantsModel.js
@@ -3,6 +3,9 @@ const { DataTypes } = require('sequelize')
 
 // TODO: Написать модель для Участников
 const ParticipantsModel = db.define('participants', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false},
+    userId: {type: DataTypes.INTEGER, allowNull: false},
+    eventId: {type: DataTypes.INTEGER, allowNull: false},
 })
 
 module.exports = ParticipantsModel;
